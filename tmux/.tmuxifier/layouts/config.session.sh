55974099c8f21467ee675a385df2615b1a4d2b5a
Binary-Blade <codegrunt@proton.me>
2024-03-31 10:45:19 +0200

    * feat: Add pane for config envman link to neovim

--- tmux/.tmuxifier/layouts/config.session.sh
+++ tmux/.tmuxifier/layouts/config.session.sh
@@ -7,6 +7,10 @@ if initialize_session "Config"; then
 
   new_window "C-Tmuxifier"
   run_cmd "cd .config/tmux/.tmuxifier/layouts && nvim ."
+
+  new_window "C-Envman"
+  run_cmd "cd .config/envman && nvim ."
+
   select_window 1
 fi
 finalize_and_go_to_session
