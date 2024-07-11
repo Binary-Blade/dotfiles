b86131fca18f3daeb9952f38ca54f0dee5e6fc6a
Binary-Blade <codegrunt@proton.me>
2024-07-11 09:29:57 +0200

    * fix: Modify path

--- tmux/.tmuxifier/layouts/config.session.sh
+++ tmux/.tmuxifier/layouts/config.session.sh
@@ -3,13 +3,13 @@ if initialize_session "Config"; then
   run_cmd "config && nvim ." || echo "Failed to run 'config && nvim .'"
 
   new_window "C-Tmux" || echo "Failed to create C-Tmux window"
-  run_cmd "cd ~/.config/tmux && nvim ." || echo "Failed to run 'cd ~/.config/tmux && nvim .'"
+  run_cmd "cd ~/dotfiles/tmux && nvim ." || echo "Failed to run 'cd ~/.config/tmux && nvim .'"
 
   new_window "C-Tmuxifier" || echo "Failed to create C-Tmuxifier window"
-  run_cmd "cd ~/.config/tmux/.tmuxifier/layouts && nvim ." || echo "Failed to run 'cd ~/.config/tmux/.tmuxifier/layouts && nvim .'"
+  run_cmd "cd ~/dotfiles/tmux/.tmuxifier/layouts && nvim ." || echo "Failed to run 'cd ~/.config/tmux/.tmuxifier/layouts && nvim .'"
 
   new_window "C-Envman" || echo "Failed to create C-Envman window"
-  run_cmd "cd ~/.config/envman && nvim ." || echo "Failed to run 'cd ~/.config/envman && nvim .'"
+  run_cmd "cd ~/dotfiles/envman && nvim ." || echo "Failed to run 'cd ~/.config/envman && nvim .'"
 
   new_window "C-Wezterm" || echo "Failed to create C-Wezterm window"
   run_cmd "cd ~/.config/wezterm && nvim ." || echo "Failed to run 'cd ~/.config/wezterm && nvim .'"
